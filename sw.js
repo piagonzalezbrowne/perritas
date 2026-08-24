ecb9da07a5dca55beb6010a43ac25db6fe24ef4e
piagonzalezbrowne <piagonzalezbrowne@gmail.com>
2026-08-24 13:48:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "perritas-v5";
+const CACHE = "perritas-v6";
 const ARCHIVOS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
